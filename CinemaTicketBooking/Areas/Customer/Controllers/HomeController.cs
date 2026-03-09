490790f073f3481bd6081671b861422ea7af3cca
Ahmedweb22 <ahmedwebdevelop2@gmail.com>
2026-03-09 07:14:31 +0200

    Work on Action Of Home Page

--- CinemaTicketBooking/Areas/Customer/Controllers/HomeController.cs
+++ CinemaTicketBooking/Areas/Customer/Controllers/HomeController.cs
@@ -8,15 +8,54 @@ namespace CinemaTicketBooking.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private IRepository<Movie> _movieRepository;
+        private IRepository<Category> _categoryRepository;
+        private IRepository<Cinema> _cinemaRepository;
+        private IRepository<Actors> _actorRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, IRepository<Movie> movieRepository, IRepository<Category> categoryRepository, IRepository<Cinema> cinemaRepository, IRepository<Actors> actorRepository )
         {
             _logger = logger;
+            _movieRepository = movieRepository;
+            _categoryRepository = categoryRepository;
+            _cinemaRepository = cinemaRepository;
+            _actorRepository = actorRepository;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int? categoryId , int? cinemaId)
         {
-            return View();
+       
+             var movies = await _movieRepository.GetAsync(m => m.Price >= 300,
+                 includes: [m => m.Category, m => m.Cinema, m => m.Actors]);
+
+           // movies = movies.Where(m => m.Price <= 300).ToList();
+           if (categoryId != null)
+            {
+                movies = movies.Where(m => m.CategoryId == categoryId).ToList();
+            }
+            if (cinemaId != null)
+            {
+                movies.Where(m => m.CinemaId == cinemaId).ToList();
+            }
+            movies = movies.Skip(0)
+                .Take(8)
+                .ToList();
+            var categories = await _categoryRepository.GetAsync();
+             var cinemas = await _cinemaRepository.GetAsync();
+            var actors = await _actorRepository.GetAsync();
+                actors = actors.Skip(0)
+                .Take(12)
+                .ToList();
+        
+             return View(new HomeVM
+             {
+                 Movies = movies.ToList(),
+                 Categories = categories.ToList(),
+                 Cinemas = cinemas.ToList(),
+                 Actors = actors.ToList()
+             });
+           
         }
 
         public IActionResult Privacy()
---
+++ CinemaTicketBooking/ViewModels/HomeVM.cs
@@ -0,0 +1,10 @@
+﻿namespace CinemaTicketBooking.ViewModels
+{
+    public class HomeVM
+    {
+        public List<Movie> Movies { get; set; }
+        public List<Category> Categories { get; set; }
+        public List<Cinema> Cinemas { get; set; }
+        public List<Actors> Actors { get; set; }
+    }
+}
